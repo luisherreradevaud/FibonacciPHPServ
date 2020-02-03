6951af6837a9579ee4cc630834169801a06695c3
luisherreradevaud <lucho.herrera@gmail.com>
2020-02-03 00:13:56 -0300

    Update serv.php

--- serv.php
+++ serv.php
@@ -1,29 +1,30 @@
 <?php
 
   if ( isset($_GET['n'])) {
-    n = $_GET['n'];
-    if ( !is_numeric(n) ) {
+    $n = $_GET['n'];
+    if ( !is_numeric($n) ) {
        response_http_code(400);
     }
   } else {
     response_http_code(400);
   }
 
-  function fibonacci(n) {
+  function fibonacci($n) {
   
-    total     = 0;
-    prev_prev = 0;
-    prev      = 1;
+    $total     = 0;
+    $prev_prev = 0;
+    $prev      = 1;
 
-    for ( i = 0; i < n ; i++ ) {
-      total = prev + prev_prev;
-      prev_prev = prev;
-      prev = total;
+    for ( $i = 0; $i < $n ; $i++ ) {
+      $total = $prev + $prev_prev;
+      $prev_prev = $prev;
+      $prev = $total;
     }
+   
+    return $total;
     
-    return total;
   }
-
-  print fibonacci(n);
+  
+  print fibonacci($n);
   
 ?>
